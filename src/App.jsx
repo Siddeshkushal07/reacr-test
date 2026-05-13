e136033ec3a690b2444fc3d49edc718754dd3e7c
Sidddesh K N <sidddeshkushal9876@gmail.com>
2026-05-13 18:42:14 +0530

    add  files to git

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Hello, World!</h1>
+      <p>This is a simple React app.</p>
+    </>
+  );
+}
+export default App;
---
+++ src/components/QueueDisplay.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function QueueDisplay() {
+  return <div></div>;
+}
+
+export default QueueDisplay;
